f1d42b4d058e3841755a6c421bf904b9d8ad47ec
ji3ee <jieee1602@gmail.com>
2025-02-14 14:15:01 +0700

    Add comment to GuestController for login check clarification

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 class GuestController extends Controller
 {
+    // ini untuk mengecek apakah user sudah login atau belum
     public function __construct()
     {
         $this->middleware('guest');
